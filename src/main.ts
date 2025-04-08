9bd2f76fe7655c8420cf99eac3669a46a4ba04f7
Mario Nachbaur <marionauta@users.noreply.github.com>
2025-04-08 13:27:44 +0200

    fix: compilation errors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import middlewares from "@/middlewares/mod.ts";
 import { htmxServe } from "deno-htmx/mod.ts";
 
 if (import.meta.main) {
-  const onListen: Deno.ServeOptions["onListen"] = ({ hostname, port }) => {
+  const onListen = (addr: Deno.NetAddr) => {
+    if (!("port" in addr)) return;
+    const { hostname, port } = addr;
     logger.info(`Listening on http://${hostname}:${port}/`);
   };
-
   const port = parseInt(Deno.env.get("PORT") || "8080", 10);
+  // @ts-ignore middlewares
   Deno.serve({ port, onListen }, htmxServe(middlewares(handlers)));
 }
